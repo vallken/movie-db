bac66787c3b6593a7e5e7486deeda338151af685
vallken <salvakennedy@gmail.com>
2024-07-22 20:31:11 +0700

    del

--- src/app/api/checkDb/route.js
+++
@@ -1,8 +0,0 @@
-import { dbConnectMovie } from "@/src/lib/mongodb";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-    await dbConnectMovie();
-    return new NextResponse("MongoDB connected");
-    // return NextResponse.json({ message: "MongoDB connected" });
-}
