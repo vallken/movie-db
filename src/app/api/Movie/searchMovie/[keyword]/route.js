e6cf8f7a86b5408e9006413d77bbdc0dcdb9a544
vallken <salvakennedy@gmail.com>
2024-07-26 12:45:41 +0700

    Fixing Some Bugs

--- src/app/api/Movie/searchMovie/[keyword]/route.js
+++ src/app/api/Movie/searchMovie/[keyword]/route.js
@@ -23,7 +23,7 @@ export async function GET(req, { params }) {
       title: { $regex: titleRegex },
     });
     const movies = await Movie.find({ title: { $regex: titleRegex } })
-      .select("-_id -__v")
+      .select("-__v")
       .skip(skip)
       .limit(limit)
       .lean();
